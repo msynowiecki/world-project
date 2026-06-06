4d69e0d45e41234abf38d141fab871f6c42b8123
msynowiecki <m.synowiecki.118@studms.ug.edu.pl>
2026-06-06 12:44:11 +0200

    feat: Add Main file

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "source/World.h"
+#include "source/models/Position.h"
+#include "source/entities/Grass.h"
+
+int main() {
+    srand(time(NULL));
+
+    World livingWorld(5, 5);
+
+    Organism* grass1 = new Grass(Position(2, 2), &livingWorld);
+    livingWorld.addOrganism(grass1);
+
+    Organism* grass2 = new Grass(Position(2, 4), &livingWorld);
+    livingWorld.addOrganism(grass2);
+
+    system("clear"); 
+
+    std::cout << livingWorld.toString() << std::endl;
+
+    for (int iterator = 0; iterator < 20; ++iterator) {
+        std::cin.get(); 
+
+        system("clear"); 
+
+        livingWorld.makeTurn();
+        std::cout << livingWorld.toString() << std::endl;
+    }
+
+    return 0;
+}
